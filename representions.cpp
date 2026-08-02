04522375cbfd6572f0128d79093b5aa9d7d98701
Serob <serob.noreyan.goya@gmail.com>
2026-08-02 11:04:04 -0600

    Representions

---
+++ representions.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+ using namespace std;
+ int main()
+ {
+    int naumber = 44;
+    cout << dec << naumber << endl;
+    cout << hex << naumber << endl;
+    cout << oct << naumber << endl;
+
+    //output: 44
+    //  2*16+13=2c 
+    //  5*8+4=54
+ }
