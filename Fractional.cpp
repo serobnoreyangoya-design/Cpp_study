e1d054b84fab1c9f2fcb7344d4b4f6521c3f12bf
Serob <serob.noreyan.goya@gmail.com>
2026-08-03 12:51:21 -0600

    Fractional numbers

---
+++ Fractional.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <iomanip>
+using namespace std;
+int main()
+{ 
+ /*
+ float  number1 = 3.1236749309939940023030;  4 bytes,7 presision
+ double number2 = 3.1236749309939940023030;  8 bytes,15 presision
+ long double number3 = 3.1236749309939940023030;  12 bytes,19 presision
+  double number4 {3.4848e-10};                 
+ //Precision of float, double and long double 
+ cout << "The value of number1 is: " << number1 << endl;
+ cout << "The value of number2 is: " << number2 << endl;
+ cout << "The value of number3 is: " << number3 << endl;
+ cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+ cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+ cout << "Size of long double: " << sizeof(long double) << " bytes" << endl;
+ cout <<  number4 << endl;
+ cout << "Size of number4: " << sizeof(number4) << " bytes"  << endl;
+ */
+
+
+
+ double number5 = -8.9;
+ double number6 {} ;
+ double number7 {};
+
+
+ double result = number5 / number6;
+ cout << "The value of result is: " << result << endl;
+ double result1 = number6 / number7;
+ cout << "The value of result1 is: " << result1 << endl;
+}
+ 
