0de7100c324489769c7da19a4c2b5a72c7c59b20
Serob <serob.noreyan.goya@gmail.com>
2026-08-03 12:56:37 -0600

    Integer numbers

---
+++ Integers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  /*
+  int a{19};
+  int b{20};
+  int h{9.9};         compiler error: narrowing conversion of ‘9.8999999999999995e+0’ 
+  int c{a + b};
+  cout << c << endl;
+  
+
+
+  int d(6);
+  int e(7);
+  int f(9.9);
+  int g(d+e);
+  cout << g << endl;
+  cout << f << endl;
+  
+
+
+  int Artashes= 13;
+  int Mane= 12;
+  int Hasmik = Artashes + Mane;
+  int Gohar = 9.9;
+  cout << Hasmik << endl;
+  cout << Gohar << endl;
+
+  // Check the size of int
+  cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+  cout  << "Size of Gohar: " << sizeof(Gohar) << " bytes" << endl;
+  */ 
+}
