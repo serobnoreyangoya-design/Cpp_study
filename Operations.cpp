014d0c9b4c4bc9b64645ea3b25f29503307b1537
Serob <serob.noreyan.goya@gmail.com>
2026-08-04 11:04:47 -0600

    Oparations

---
+++ Operations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+ int a,b,c,d,e,f,g;
+ a = 14;
+ b =  6;
+ c = 9;
+ d = 3;
+ e = 12;
+ f = 5;
+ g = 1;
+
+ int result = a+b/d-c*e-f+g;
+ cout << "result = " << result << endl; 
+
+
+
+  result = a+b/d-c*e-(f+g);
+   cout << "result = " << result << endl; 
+
+}
+ 
