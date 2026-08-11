ab8d97d3ae5dc3bde3616ffd2b52d2c6d49a5e19
Serob <serob.noreyan.goya@gmail.com>
2026-08-11 13:58:40 -0600

    Masks example

---
+++ Masksexpl.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+void number (bool flag0 , bool flag1,bool flag2,bool flag3,bool flag4,bool flag5)
+{
+
+     cout << boolalpha;
+     cout << "flag0 = " << flag0 << endl;
+     cout << "flag1 = " << flag1 << endl;
+     cout << "flag2 = " << flag2 << endl;
+     cout << "flag3 = " << flag3 << endl;
+     cout << "flag4 = " << flag4 << endl;
+     cout << "flag5 = " << flag5 << endl;
+
+
+}
+
+int main()
+{
+
+     number (0,0,1,1,1,0);
+
+     
+}
