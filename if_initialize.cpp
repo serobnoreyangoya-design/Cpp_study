ef9442e12dec8fb070c488789c5207e1685ee515
Serob Noreyan <serob.noreyan.goya@gmail.com>
2026-08-19 15:40:04 +0400

    if initialize

---
+++ if_initialize.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+bool stop = true;
+
+if (int speed = 90;stop){
+    if(speed > 60){
+      cout << "slow down" << endl;
+    }
+    else{
+        cout << "keep going" << endl;
+    }
+}
+else{
+cout << "go" << endl;
+}
+//cout << speed << endl; compiler error
+
+}
