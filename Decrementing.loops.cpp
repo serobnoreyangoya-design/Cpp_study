054e70016b07f60b81ff09467d97a255efc17abd
Serob Noreyan <serob.noreyan.goya@gmail.com>
2026-08-26 22:08:01 +0400

    .

---
+++ Decrementing.loops.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+//     for loop 
+
+    /* 
+size_t count = 20 ; 
+  int  i = 0;
+for (  ; i < count ; i++ ){
+    cout << i << endl;
+}
+
+for(i  ; i > 0 ; i--){
+    cout << i << endl;
+}
+*/
+
+
+//   while loop
+
+/*
+int  i = 0 ;
+size_t count = 10;
+
+while(i < count){
+   cout << i << endl;
+   i++;
+}
+
+
+
+while ( i >= 0 ){
+    cout << i << endl;
+    --i;
+}
+*/
+
+// DO while loop
+
+int  i = 0;
+size_t count = 15;
+
+do{
+    cout << i << endl;
+    i++;
+}while(i < count);
+
+
+do{
+    cout << i << endl;
+    i--;
+}while( i  < count );
+
+
+}
